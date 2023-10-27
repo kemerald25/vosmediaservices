99d823a15b8369355050245262d58313c9def76b
GiftAni <goldengirlonyiyechi@gmail.com>
2023-10-27 10:09:36 +0100

    update

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,18 +1,18 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
 
-let firebaseConfig = {
-    apiKey: "AIzaSyCvm4iiArQcMyOXYU4r5vStYkWnShARgNc",
-    authDomain: "blogging-site-f4df3.firebaseapp.com",
-    projectId: "blogging-site-f4df3",
-    storageBucket: "blogging-site-f4df3.appspot.com",
-    messagingSenderId: "606776501559",
-    appId: "1:606776501559:web:097196365b1a771885a6b2",
-    measurementId: "G-MSVV9TTQSY",
-};
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-// firebase.initializeApp(firebaseConfig); 
+// let firebaseConfig = {
+//     apiKey: "AIzaSyCvm4iiArQcMyOXYU4r5vStYkWnShARgNc",
+//     authDomain: "blogging-site-f4df3.firebaseapp.com",
+//     projectId: "blogging-site-f4df3",
+//     storageBucket: "blogging-site-f4df3.appspot.com",
+//     messagingSenderId: "606776501559",
+//     appId: "1:606776501559:web:097196365b1a771885a6b2",
+//     measurementId: "G-MSVV9TTQSY",
+// };
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+// // firebase.initializeApp(firebaseConfig); 
 
-// let db = firebase.firestore();
+// // let db = firebase.firestore();
