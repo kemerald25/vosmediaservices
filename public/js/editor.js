3a6d3c75fee43bc91b8e3ea8cb8c71b0fe3c630e
GiftAni <goldengirlonyiyechi@gmail.com>
2023-10-28 02:14:49 +0100

    update

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,70 +1,81 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const blogTitleField = document.querySelector('.title');
-    const articleField = document.querySelector('.article');
-    const bannerImageInput = document.querySelector('#banner-upload');
-    const publishBtn = document.querySelector('.publish-btn');
 
-    bannerImageInput.addEventListener('change', () => {
-        uploadImage(bannerImageInput, "banner");
-    });
+const blogTitleField = document.querySelector('.title');
+const articleFeild = document.querySelector('.article');
 
-    const uploadImage = (uploadFile, uploadType) => {
-        const [file] = uploadFile.files;
-        if (file && file.type.includes("image")) {
-            const formData = new FormData();
-            formData.append('image', file);
+// banner
+const bannerImage = document.querySelector('#banner-upload');
+const banner = document.querySelector(".banner");
+let bannerPath;
 
-            fetch('/upload', {
-                method: 'POST',
-                body: formData
-            })
-            .then(response => response.json())
-            .then(data => {
-                if (uploadType === "banner") {
-                    // Handle banner image upload logic if needed
-                } else {
-                    // Handle other image upload logic if needed
-                }
-            })
-            .catch(error => {
-                console.error('Error uploading image:', error);
-            });
-        } else {
-            alert("Upload image files only");
+const publishBtn = document.querySelector('.publish-btn');
+const uploadInput = document.querySelector('#image-upload');
+
+bannerImage.addEventListener('change', () => {
+    uploadImage(bannerImage, "banner");
+})
+
+uploadInput.addEventListener('change', () => {
+    uploadImage(uploadInput, "image");
+})
+
+const uploadImage = (uploadFile, uploadType) => {
+    const [file] = uploadFile.files;
+    if(file && file.type.includes("image")){
+        const formdata = new FormData();
+        formdata.append('image', file);
+
+        fetch('/upload', {
+            method: 'post',
+            body: formdata
+        }).then(res => res.json())
+        .then(data => {
+            if(uploadType == "image"){
+                addImage(data, file.name);
+            } else{
+                bannerPath = `${location.origin}/${data}`;
+                banner.style.backgroundImage = `url("${bannerPath}")`;
+            }
+        })
+    } else{
+        alert("upload Image only");
+    }
+}
+
+const addImage = (imagepath, alt) => {
+    let curPos = articleFeild.selectionStart;
+    let textToInsert = `\r![${alt}](${imagepath})\r`;
+    articleFeild.value = articleFeild.value.slice(0, curPos) + textToInsert + articleFeild.value.slice(curPos);
+}
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+publishBtn.addEventListener('click', () => {
+    if(articleFeild.value.length && blogTitleField.value.length){
+        // generating id
+        let letters = 'abcdefghijklmnopqrstuvwxyz';
+        let blogTitle = blogTitleField.value.split(" ").join("-");
+        let id = '';
+        for(let i = 0; i < 4; i++){
+            id += letters[Math.floor(Math.random() * letters.length)];
         }
-    };
 
-    publishBtn.addEventListener('click', () => {
-        const title = blogTitleField.value;
-        const article = articleField.value;
-        const bannerImage = ''; // Set the banner image URL if applicable
-        const publishedAt = new Date().toLocaleString(); // Get current timestamp
+        // setting up docName
+        let docName = `${blogTitle}-${id}`;
+        let date = new Date(); // for published at info
 
-        if (title && article) {
-            const postData = {
-                title: title,
-                article: article,
-                bannerImage: bannerImage,
-                publishedAt: publishedAt
-            };
-
-            fetch('/publish', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(postData)
-            })
-            .then(response => response.json())
-            .then(data => {
-                // Handle the response from the server, if needed
-                console.log(data);
-            })
-            .catch(error => {
-                console.error('Error publishing blog post:', error);
-            });
-        } else {
-            alert('Please enter a title and article content');
-        }
-    });
-});
+        //access firstore with db variable;
+        db.collection("blogs").doc(docName).set({
+            title: blogTitleField.value,
+            article: articleFeild.value,
+            bannerImage: bannerPath,
+            publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+        })
+        .then(() => {
+            // location.href = `/${docName}`;
+            console.log("it is done")
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+    }
+})
--- server.js
+++ server.js
@@ -1,70 +1,49 @@
 const express = require('express');
 const path = require('path');
 const fileupload = require('express-fileupload');
-const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/blogDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+let initial_path = path.join(__dirname, "public");
 
 const app = express();
-const initial_path = path.join(__dirname, "public");
-
 app.use(express.static(initial_path));
 app.use(fileupload());
-app.use(express.json());
-
-// MongoDB schema for blog posts
-const blogSchema = new mongoose.Schema({
-    title: String,
-    article: String,
-    bannerImage: String,
-    publishedAt: String
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(initial_path, "home.html"));
-});
+})
 
 app.get('/editor', (req, res) => {
     res.sendFile(path.join(initial_path, "editor.html"));
-});
+})
 
-app.post('/uploads', (req, res) => {
-    // Handle image upload logic similar to your previous implementation
-    // ...
-});
+// upload link
+app.post('/upload', (req, res) => {
+    let file = req.files.image;
+    let date = new Date();
+    // image name
+    let imagename = date.getDate() + date.getTime() + file.name;
+    // image upload path
+    let path = 'public/uploads/' + imagename;
 
-app.post('/publish', (req, res) => {
-    const { title, article, bannerImage, publishedAt } = req.body;
-    
-    const newBlog = new Blog({
-        title: title,
-        article: article,
-        bannerImage: bannerImage,
-        publishedAt: publishedAt
-    });
-
-    newBlog.save((err) => {
-        if (err) {
-            res.status(500).json({ error: 'Error saving blog post' });
-        } else {
-            res.status(200).json({ message: 'Blog post saved successfully' });
+    // create upload
+    file.mv(path, (err, result) => {
+        if(err){
+            throw err;
+        } else{
+            // our image upload path
+            res.json(`uploads/${imagename}`)
         }
-    });
-});
+    })
+})
 
 app.get("/:blog", (req, res) => {
     res.sendFile(path.join(initial_path, "blog.html"));
-});
+})
 
 app.use((req, res) => {
-    res.status(404).json("404");
-});
+    res.json("404");
+})
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
-});
+app.listen("4000", () => {
+    console.log('listening......');
+})
